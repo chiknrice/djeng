704d5c2259cde6d270950fb50956252efa63e43b
ian <chiknrice@gmail.com>
2016-07-25 16:59:18 +1200

    fixed track-2 packed bug

--- src/main/java/org/chiknrice/djeng/fin/Track2Codec.java
+++ src/main/java/org/chiknrice/djeng/fin/Track2Codec.java
@@ -14,6 +14,9 @@ public class Track2Codec extends StringCodec implements LengthPrefixDelegate {
         Boolean packed = getAttribute(PACKED);
         if (packed) {
             value = value.replace('=', 'D');
+            if (value.length() % 2 > 0) {
+                value = value.concat("F");
+            }
             return ByteUtil.decodeHex(value);
         } else {
             return super.encodeValue(value);
@@ -25,6 +28,9 @@ public class Track2Codec extends StringCodec implements LengthPrefixDelegate {
         Boolean packed = getAttribute(PACKED);
         if (packed) {
             String packedTrack2 = ByteUtil.encodeHex(bytes);
+            if (packedTrack2.endsWith("F")) {
+                packedTrack2 = packedTrack2.substring(0, packedTrack2.length() - 1);
+            }
             return packedTrack2.replace('D', '=');
         } else {
             return super.decodeValue(bytes);
